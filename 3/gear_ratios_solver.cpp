0b52eb4da890925ef09c9ee576c03ac5268e9d23
MattJTrueblood <mattjtrueblood@gmail.com>
2023-12-04 00:28:25 +0530

    adding part 2 implementation

--- 3/gear_ratios_solver.cpp
+++ 3/gear_ratios_solver.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-static const bool PART_2_ENABLED = false;
+static const bool PART_2_ENABLED = true;
 
 // converts a filename to a string vector of all the lines in the file
 vector<string> parseFile(char* filename) {
@@ -53,6 +53,11 @@ int charVectorToInt(vector<char> vec) {
 // true if c is any character except 0-9 or period.
 bool isSymbol(char c) {
     return !(isdigit(c) || c == '.');
+}
+
+// checks if it's a gear character (checking for 2 adjacent numbers is handled elsewhere)
+bool isGear(char c) {
+    return c == '*';
 }
 
 // find all in bounds neighbors of grid[i][k]
@@ -145,6 +150,135 @@ int getSumOfAllPartNumbers(vector<vector<char>> grid) {
     return sum;
 }
 
+//uses a part num grid and i,j coordinates to find the gear ratio for a gear at that tile (product of adjacent numbers)
+int calculateGearRatio(vector<vector<int>> grid, int i, int j) {
+    //check bounds
+    bool rightIsValid = j < grid[0].size() - 1;
+    bool upIsValid = i > 0;
+    bool leftIsValid = j > 0;
+    bool downIsValid = i < grid.size() - 1;
+
+    bool foundGap = false;
+    bool lookingForNumOne = true;
+
+    int numOne = -1;
+    int numTwo = -1;
+
+    int neighbors[] = { grid[i-1][j-1], grid[i-1][j], grid[i-1][j+1],
+                        grid[i][j-1],                 grid[i][j+1],
+                        grid[i+1][j-1], grid[i+1][j], grid[i+1][j+1] };
+
+    // make sure you get separate numbers.  This is surprisingly annoying to do and requires checking gaps and line breaks and such
+    // in the rare possible case that the numOne and numTwo are the equal but separate numbers on the grid.  This is a consequence of
+    // the hacky way we're doing this where the same number is repeated in every tile it's located in in the grid here, so we need to 
+    // be able to distinguish between true duplicates and false duplicates.  
+    for(int k = 0; k < 8; k++) {
+        if(lookingForNumOne) {
+            if(neighbors[k] != 0) {
+                numOne = neighbors[k];
+                lookingForNumOne = false;
+            }
+        }
+        else if(!lookingForNumOne) {
+            if(!foundGap) {
+                if(k == 3 || k == 4 || k == 5) {
+                    //natural gap (e.g. neighbor 3, to the left, is always a different number from neighbor 2, to the up/right)
+                    foundGap = true;
+                }
+                if(neighbors[k] == 0) {
+                    // normal gap
+                    foundGap = true;
+                }
+            }
+            if(foundGap && neighbors[k] != 0) {
+                //std::cout << "num two set to " << neighbors[k] << endl;;
+                numTwo = neighbors[k];
+                break;
+            }
+        }
+    }
+
+    //if only one neighbor, it's not a real gear.
+    if(numOne == -1 || numTwo == -1) {
+        return 0;
+    }
+
+    return numOne * numTwo;
+}
+
+// find all gear ratio sums
+int getSumOfAllGearRatios(vector<vector<char>> grid) {
+
+    vector<char> currentNumberVector;
+    bool currentlyReadingNumber = false;
+    bool currentNumberIsPartNumber = false;
+    int currentNumberDigitCount = 0;
+
+    // this vector is used to track part nums at [i][j]
+    // kinda hacky way to make this work with my existing part 1 implementation
+    // 0 = no digit.  >0 = digit (same for each tile the digit is in).  -1 = gear
+    vector<vector<int>> partNumGrid;
+
+    for(int i= 0; i < grid.size(); i++) {
+        vector<int> partNumGridLine;
+        for(int j = 0; j < grid[0].size(); j++) {
+            char c = grid[i][j];
+            int currentPartGridValue = 0; // no part set here yet.
+            if(isdigit(c)) {
+                currentNumberDigitCount++;
+                currentNumberVector.push_back(c);
+                currentlyReadingNumber = true;
+                if(!currentNumberIsPartNumber) {
+                    if(hasAdjacentSymbol(grid, i, j)) {
+                        currentNumberIsPartNumber = true;
+                    }
+                }
+            } else {
+                if(currentlyReadingNumber) {
+                    int currentNumber = charVectorToInt(currentNumberVector);
+                    if(currentNumberIsPartNumber) {
+                        int currentNumber = charVectorToInt(currentNumberVector);
+
+                        //set all previous partNumGrid tiles this number is in to currentNumber
+                        if(j != 0) { //normal case, update the previous values in this line 
+                            for(int k = 0; k < currentNumberDigitCount; k++) {
+                                partNumGridLine[partNumGridLine.size() - 1 - k] = currentNumber;
+                            }
+                        } else { //special case, this is the first column, we need to set the value from the end of the previous line
+                            for(int k = 0; k < currentNumberDigitCount; k++) {
+                                partNumGrid[partNumGrid.size() - 1][partNumGrid[0].size() - 1 - k] = currentNumber;
+                            }    
+                        }
+                    }
+                    currentNumberVector.clear();
+                    currentlyReadingNumber = false;
+                    currentNumberIsPartNumber = false;
+                    currentNumberDigitCount = 0;
+                }
+                if(isGear(c)) {
+                    currentPartGridValue = -1;
+                }
+            }
+            partNumGridLine.push_back(currentPartGridValue);
+        }
+        partNumGrid.push_back(partNumGridLine);
+    }
+
+    //now go through the partnum grid and sum the gear ratios for each gear you find there
+    int gearRatioSum = 0;
+    for(int i= 0; i < partNumGrid.size(); i++) {
+        for(int j = 0; j < partNumGrid[0].size(); j++) {
+            int n = partNumGrid[i][j];
+            if(n == -1) {
+                // gear tile
+                gearRatioSum += calculateGearRatio(partNumGrid, i, j);
+            }
+        }
+    }
+
+    return gearRatioSum;
+}
+
 int main(int argc, char* argv[]) {
     // check usage correctness
     if (argc < 2) {
@@ -158,7 +292,12 @@ int main(int argc, char* argv[]) {
 
     // calculate result
     vector<vector<char>> grid = generateGrid(lines);
-    int sum = getSumOfAllPartNumbers(grid);
+    int sum;
+    if(PART_2_ENABLED) {
+        sum = getSumOfAllGearRatios(grid);
+    } else {
+        sum = getSumOfAllPartNumbers(grid);
+    }   
 
     // print final result
     cout << "sum=" << sum << endl;
