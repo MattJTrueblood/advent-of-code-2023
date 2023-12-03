5e61d328c28c1a0b8e83ee4da60f630760033ab6
MattJTrueblood <mattjtrueblood@gmail.com>
2023-12-03 21:13:23 +0530

    adding solver for day 3

--- 3/gear_ratios_solver.cpp
+++ 3/gear_ratios_solver.cpp
@@ -7,14 +7,11 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-static const bool PART_2_ENABLED = true;
-
-static const int MAX_RED_CUBES = 12;
-static const int MAX_GREEN_CUBES = 13;
-static const int MAX_BLUE_CUBES = 14;
+static const bool PART_2_ENABLED = false;
 
 // converts a filename to a string vector of all the lines in the file
 vector<string> parseFile(char* filename) {
@@ -27,113 +24,143 @@ vector<string> parseFile(char* filename) {
     return lines;
 }
 
-//return the id of the game at this line if it's impossible, otherwise return 0
-int getGameIdIfImpossible(string &str) {
-    // add a termination character to simplify the loop
-    str = str + ";";
-
-    // setup string stream for parsing this line
-    istringstream stringStream(str);
-    string word;
-
-    getline(stringStream, word, ' '); // skip the first line, "Game"
-    getline(stringStream, word, ' '); // now `word` is the game id
-    string gameIdStr = word.substr(0, word.size() - 1); // strip the colon from the end
-    int gameId = stoi(gameIdStr); // parse as integer
-
-    string countStr;
-    string color;
-    int count;
-
-    // count and color will always come in pairs from now on
-    while(getline(stringStream, countStr,' ')) {
-        getline(stringStream, color, ' ');
-
-        // parse color and count properly 
-        color = color.substr(0, color.size() - 1);
-        count = stoi(countStr);
-
-        //now check against predefined limits
-        if(color == "blue" && count > MAX_BLUE_CUBES) {
-            return 0;
+//converts lines into a 2d grid of chars (probably not needed?  Just to be safe)
+vector<vector<char>> generateGrid(vector<string> &lines) {
+    vector<vector<char>> grid;
+    for(int i = 0; i < lines.size(); i++) {
+        vector<char> gridLine;
+        for(int j = 0; j < lines[i].size(); j++) {
+            gridLine.push_back(lines[i][j]);
         }
-        if(color == "green" && count > MAX_GREEN_CUBES) {
-            return 0;
-        }
-        if(color == "red" && count > MAX_RED_CUBES) {
-            return 0;
-        }
+        grid.push_back(gridLine);
     }
-
-    // this game is possible
-    return gameId;
+    return grid;
 }
 
-// return the minimum red, green, and blue counts of the game multiplied together
-int getPowerOfGame(string &str) {
-    // add a termination character to simplify the loop
-    str = str + ";";
+// converts a char vector to the equivalent integer
+int charVectorToInt(vector<char> vec) {
+    int num = 0;
+    int mod = 1;
+    for(int i = 0; i < vec.size(); i++) {
+        char c = vec[(vec.size() - 1) - i];
+        int digit = c - '0';
+        num += (digit * mod);
+        mod = mod * 10;
+    }
+    return num;
+}
 
-    // setup string stream for parsing this line
-    istringstream stringStream(str);
-    string word;
+// true if c is any character except 0-9 or period.
+bool isSymbol(char c) {
+    return !(isdigit(c) || c == '.');
+}
 
-    getline(stringStream, word, ' '); // skip the first line, "Game"
-    getline(stringStream, word, ' '); // now `word` is the game id
+// find all in bounds neighbors of grid[i][k]
+// return true if any of them are symbols, else false
+bool hasAdjacentSymbol(vector<vector<char>> grid, int i, int j) {
+    vector<char> charsToCheck;
 
-    string countStr;
-    string color;
-    int count;
+    //check bounds
+    bool rightIsValid = j < grid[0].size() - 1;
+    bool upIsValid = i > 0;
+    bool leftIsValid = j > 0;
+    bool downIsValid = i < grid.size() - 1;
 
-    int minRed = 0;
-    int minBlue = 0;
-    int minGreen = 0;
-
-    // count and color will always come in pairs from now on
-    while(getline(stringStream, countStr,' ')) {
-        getline(stringStream, color, ' ');
-
-        // parse color and count properly 
-        color = color.substr(0, color.size() - 1);
-        count = stoi(countStr);
-
-        //now check against predefined limits
-        if(color == "blue" && count > minBlue) {
-            minBlue = count;
+    //add all in-bounds neighbors
+    if(upIsValid) {
+        charsToCheck.push_back(grid[i-1][j]);
+        if(leftIsValid) {
+            charsToCheck.push_back(grid[i-1][j-1]);
         }
-        if(color == "green" && count > minGreen) {
-            minGreen = count;
+        if(rightIsValid) {
+            charsToCheck.push_back(grid[i-1][j+1]);
         }
-        if(color == "red" && count > minRed) {
-            minRed = count;
+    }
+    if(leftIsValid) {
+        charsToCheck.push_back(grid[i][j-1]);
+    }
+    if(rightIsValid) {
+        charsToCheck.push_back(grid[i][j+1]);
+    }
+    if(downIsValid) {
+        charsToCheck.push_back(grid[i+1][j]);
+        if(leftIsValid) {
+            charsToCheck.push_back(grid[i+1][j-1]);
+        }
+        if(rightIsValid) {
+            charsToCheck.push_back(grid[i+1][j+1]);
         }
     }
 
-    return minRed * minBlue * minGreen;
+    // return true if any of those chars are symbols
+    for(int k=0; k < charsToCheck.size(); k++) {
+        if(isSymbol(charsToCheck[k])) {
+            return true;
+        }
+    }
+
+    // no adjacent symbols
+    return false;
+}
+
+// goes through the grid to find all part numbers (numbers with adjacent symbols)
+// convert the part numbers to numbers and sum them up.  Return the sum.
+int getSumOfAllPartNumbers(vector<vector<char>> grid) {
+
+    vector<char> currentNumberVector;
+    bool currentlyReadingNumber = false;
+    bool currentNumberIsPartNumber = false;
+    int sum = 0;
+
+    for(int i= 0; i < grid.size(); i++) {
+        for(int j = 0; j < grid[0].size(); j++) {
+            char c = grid[i][j];
+            //cout << c;
+            if(isdigit(c)) {
+                currentNumberVector.push_back(c);
+                currentlyReadingNumber = true;
+                if(!currentNumberIsPartNumber) {
+                    if(hasAdjacentSymbol(grid, i, j)) {
+                        currentNumberIsPartNumber = true;
+                    }
+                }
+            } else {
+                if(currentlyReadingNumber) {
+                    int currentNumber = charVectorToInt(currentNumberVector);
+                    if(currentNumberIsPartNumber) {
+                        int currentNumber = charVectorToInt(currentNumberVector);
+                        //cout << currentNumber << " is a part number" << endl;
+                        sum += currentNumber;
+                    } else {
+                        int currentNumber = charVectorToInt(currentNumberVector);
+                        //cout << currentNumber << " is NOT a part number" << endl;
+                    }
+                    currentNumberVector.clear();
+                    currentlyReadingNumber = false;
+                    currentNumberIsPartNumber = false;
+                }
+            }
+        }
+    }
+    return sum;
 }
 
 int main(int argc, char* argv[]) {
-    //check usage correctness
+    // check usage correctness
     if (argc < 2) {
         cerr << "Usage: " << argv[0] << " <filename>\n";
         return 1;
     }
     cout << "Filename: " << argv[1] << endl;
 
-    //parse file
+    // parse file
     vector<string> lines = parseFile(argv[1]);
 
-    // compute sum of possible game ids
-    int sum = 0;
-    for(int i = 0; i < lines.size(); i++) {
-        if(PART_2_ENABLED) {
-            sum += getPowerOfGame(lines[i]);
-        } else {
-            sum += getGameIdIfImpossible(lines[i]);
-        }
-    }
+    // calculate result
+    vector<vector<char>> grid = generateGrid(lines);
+    int sum = getSumOfAllPartNumbers(grid);
 
-    //print final result
+    // print final result
     cout << "sum=" << sum << endl;
 
     return 0;
