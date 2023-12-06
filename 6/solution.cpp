55afddfbc9dabfcaeb0b4b9f10c7a5828da6e4c8
MattJTrueblood <mattjtrueblood@gmail.com>
2023-12-06 16:51:18 +0530

    adding day 6 part 2

--- 6/solution.cpp
+++ 6/solution.cpp
@@ -12,21 +12,24 @@
 using namespace std;
 
 // set this to false to get part 1 results instead
-static const bool PART_2_ENABLED = false;
+static const bool PART_2_ENABLED = true;
 
-int getNumWinConditions(int time, int distance) {
-    int winCount = 0;
-    for(int i = 0; i <= time; i++) {
-        int timeCharging = i;
-        int timeTraveling = time - i;
+//loops through all possible hold/travel times and computes how many possible win conditions there are
+//This is surprisingly fast, times under 1,000,000,000 are totally manageable.
+//It's possible to do this more efficiently with quadratric formula or efficient searching, but it's honestly unnecessary.
+long getNumWinConditions(long time, long distance) {
+    long winCount = 0;
+    for(long i = 0; i <= time; i++) {
+        long timeCharging = i;
+        long timeTraveling = time - i;
         if(timeCharging * timeTraveling > distance) {
-            cout << "wait " << timeCharging << ", travel " << timeTraveling << endl;
             winCount++;
         }
     }
     return winCount;
 }
 
+// Part 1 solution
 int getProductOfPossibleWinConditions(vector<vector<string>> wordsInLines) {
     vector<string> timesWords = wordsInLines[0];
     vector<string> distancesWords = wordsInLines[1];
@@ -36,10 +39,28 @@ int getProductOfPossibleWinConditions(vector<vector<string>> wordsInLines) {
         int currentTime = stoi(timesWords[i]);
         int currentDistance = stoi(distancesWords[i]);
         int wins = getNumWinConditions(currentTime, currentDistance);
-        cout << "wins: " << wins << endl;
         product = product * wins;
     }
     return product;
+}
+
+// Part 2 solution
+long getPossibleWinConditionsPart2(vector<vector<string>> wordsInLines) {
+    // combine all the times and distance number strings to get one big time and one big distance
+    vector<string> timesWords = wordsInLines[0];
+    vector<string> distancesWords = wordsInLines[1];
+    string combinedTimes = "";
+    string combinedDistances = "";
+    for(int i = 1; i < timesWords.size(); i++) {
+        combinedTimes = combinedTimes + timesWords[i];
+        combinedDistances = combinedDistances + distancesWords[i];
+    }
+    //parse the string, we need to use long because it is pretty huge
+    long combinedTime = stol(combinedTimes);
+    long combinedDistance = stol(combinedDistances);
+
+    //Now simply run getNumWinConditions normally on it.
+    return getNumWinConditions(combinedTime, combinedDistance);
 }
 
 int main(int argc, char* argv[]) {
@@ -55,9 +76,9 @@ int main(int argc, char* argv[]) {
     vector<vector<string>> wordsInLines = parseAllLines(lines);
 
     // now handle lines to generate the result
-    int result;
+    long result;
     if(PART_2_ENABLED) {
-        result = -1; //todo
+        result = getPossibleWinConditionsPart2(wordsInLines);
     } else {
         result = getProductOfPossibleWinConditions(wordsInLines);
     }
